7dc0f672d47c77579141c5f2229061d3cc840342
jeon-eunseong <dmstjd1024@naver.com>
2023-02-24 00:01:00 +0900

    [master] 프로젝트 초기화

---
+++ src/main/java/com/yeolsimee/moneysaving/MoneySavingServerApplication.java
@@ -0,0 +1,13 @@
+package com.yeolsimee.moneysaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySavingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneySavingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeolsimee/moneysaving/MoneySavingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeolsimee.moneysaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySavingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
