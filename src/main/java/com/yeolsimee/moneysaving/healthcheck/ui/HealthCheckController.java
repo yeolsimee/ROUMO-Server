692be924b6236b07c75bd653feed1b2b26c4ab01
Sangmin <sm9171@nate.com>
2023-02-26 17:12:46 +0900

    feat(healthcheck):상태체크 api 및 인수테스트 작성

---
+++ src/main/java/com/yeolsimee/moneysaving/healthcheck/ui/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.yeolsimee.moneysaving.healthcheck.ui;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
+    }
+}
