e4a9e6363bce2efa4df621afb09f2df3a7164099
jeon-eunseong <dmstjd1024@naver.com>
2023-03-04 16:23:09 +0900

    feat(register): 회원가입 요청 URL 허용

--- src/main/java/com/yeolsimee/moneysaving/config/SecurityConfig.java
+++ src/main/java/com/yeolsimee/moneysaving/config/SecurityConfig.java
@@ -45,6 +45,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
                     authorize.antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                             .antMatchers(HttpMethod.POST, "/api/v1/signin").permitAll()
+                            .antMatchers(HttpMethod.POST, "/api/v1/signup").permitAll()
                             .antMatchers(HttpMethod.GET, "/healthcheck").permitAll()
                             .anyRequest().authenticated()
             );
