f445c0d4c109bb34343d7cdffc6969ba431b6b16
Sangmin <sm9171@nate.com>
2023-03-27 00:56:49 +0900

    ci: 배포 소스 수정

--- src/main/java/com/yeolsimee/moneysaving/config/FirebaseConfig.java
+++ src/main/java/com/yeolsimee/moneysaving/config/FirebaseConfig.java
@@ -1,14 +1,17 @@
 package com.yeolsimee.moneysaving.config;
 
-import com.google.auth.oauth2.*;
-import com.google.firebase.*;
-import com.google.firebase.auth.*;
-import lombok.*;
-import lombok.extern.slf4j.*;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * packageName    : com.yeolsimee.moneysaving.config
@@ -33,11 +36,10 @@ public class FirebaseConfig {
     public FirebaseApp firebaseApp() throws IOException {
         System.out.println(firebaseSdkPath);
         log.info("Initializing Firebase.");
-        FileInputStream serviceAccount =
-                new FileInputStream(firebaseSdkPath);
+        InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(firebaseSdkPath);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(resourceAsStream))
                 .build();
 
         FirebaseApp app = FirebaseApp.initializeApp(options);
