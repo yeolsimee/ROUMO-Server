4c7205be3b92ca8b792a748a36905af16224d18d
Sangmin <sm9171@nate.com>
2023-05-15 13:50:02 +0900

    fix: AlarmStatus가 ON 인것만 조회하기

--- src/main/java/com/yeolsimee/moneysaving/app/routine/repository/RoutineRepository.java
+++ src/main/java/com/yeolsimee/moneysaving/app/routine/repository/RoutineRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface RoutineRepository extends JpaRepository<Routine, Long>, RoutineRepositoryCustom {
     Optional<Routine> findByIdAndUserId(Long routineId, Long userId);
 
-    @Query("select r from Routine r where r.user.id = :userId and r.routineStartDate <= :routineDay and r.routineEndDate >= :routineDay")
+    @Query("select r from Routine r where r.user.id = :userId and r.routineStartDate <= :routineDay and r.routineEndDate >= :routineDay and r.alarmStatus = 'ON'")
     List<Routine> findByUserId(Long userId, String routineDay);
 
     @Query("select count (r) from Routine r left JOIN r.weekTypes w where r.user.id = :userId and ((r.routineStartDate <= :routineDay and r.routineEndDate >= :routineDay and w = :weekType) or (r.routineStartDate = :routineDay and r.routineEndDate = :routineDay))")
