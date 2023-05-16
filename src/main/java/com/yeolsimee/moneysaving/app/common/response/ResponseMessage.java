0fab8a8d1c7865a6c47a6da6e92f878ecb1471fc
jeon-eunseong <dmstjd1024@naver.com>
2023-05-17 02:31:27 +0900

    feature(withdraw): 회원탈퇴 메시지값 변경

--- src/main/java/com/yeolsimee/moneysaving/app/common/response/ResponseMessage.java
+++ src/main/java/com/yeolsimee/moneysaving/app/common/response/ResponseMessage.java
@@ -24,6 +24,7 @@ public enum ResponseMessage {
 
     // USER 에러코드
     AUTH_USER("사용자 정보가 없습니다."),
+    WITHDRAW_USER("탈퇴한 회원입니다."),
 
     // ROUTINE 에러코드
     NOT_VALID_ROUTINE("해당 루틴이 없습니다."),
@@ -43,8 +44,6 @@ public enum ResponseMessage {
     EXPIRED_JWT_TOKEN("만료된 토큰입니다."),
     INVALID_ID_TOKEN("유효하지 않은 토큰입니다."),
 
-    //탈퇴한 회원
-    WITHDRAW("탈퇴한 회원입니다.")
 
     ;
     final String message;
